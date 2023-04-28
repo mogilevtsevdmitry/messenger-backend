27449b5c8b744699349a147fa39ba76298a4e3cc
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-04-29 00:04:47 +0500

    add shared library

---
+++ libs/shared/src/shared.module.ts
@@ -0,0 +1,8 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Global()
+@Module({
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
+})
+export class SharedModule {}
