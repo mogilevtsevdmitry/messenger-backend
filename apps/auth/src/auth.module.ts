a2a7f72c2829dc69481c072c89fd5b847ee46d1d
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-04-29 00:04:57 +0500

    upd auth

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { SharedModule } from '@shared';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+    imports: [SharedModule],
 })
 export class AuthModule {}
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+    const config = new ConfigService();
+    const port = config.get<number>('AUTH_API_PORT', 5001);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
+        transport: Transport.TCP,
+        options: {
+            port,
+        },
+    });
+    await app.listen().then(() => {
+        Logger.log(`AuthMicroservice started on "${port}"`, 'AUTH');
+    });
 }
 bootstrap();
