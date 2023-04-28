3cfa447ef2d8c2bafd35289b43848d2a4d491515
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-04-29 00:05:05 +0500

    update main

--- apps/messenger-backend/src/app.module.ts
+++ apps/messenger-backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { SharedModule } from '@shared';
 
 @Module({
-    imports: [],
+    imports: [SharedModule],
 })
 export class AppModule {}
--- apps/messenger-backend/src/main.ts
+++ apps/messenger-backend/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const config = app.get(ConfigService);
+    const port = config.get('API_PORT', 5000);
+    await app.listen(port, () => {
+        Logger.log(`Main app started on "${port}" port`, 'Main');
+    });
 }
 bootstrap();
