f9344c7798ce9fa47df5d9e615eb85025703ad00
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-04-30 10:39:56 +0500

    rm local login

--- apps/auth/src/local/local.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoginController } from './login/login.controller';
-import { LoginService } from './login/login.service';
-
-@Module({
-    controllers: [LoginController],
-    providers: [LoginService],
-})
-export class LocalModule {}
