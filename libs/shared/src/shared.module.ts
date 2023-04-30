bfd79e6045d88109d2d054f51f13a82633e9205f
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-04-30 10:40:56 +0500

    upd imports

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -1,8 +1,20 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard, RolesGuard } from './guards';
 
 @Global()
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true })],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: JwtGuard,
+        },
+        {
+            provide: APP_GUARD,
+            useClass: RolesGuard,
+        },
+    ],
 })
 export class SharedModule {}
