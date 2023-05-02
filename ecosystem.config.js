e8c64a5a28af672762915c66a782e13429dc717a
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-05-02 14:24:32 +0500

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    apps: [
+        {
+            name: 'main',
+            script: './dist/apps/gateaway/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'auth',
+            script: './dist/apps/auth/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'user',
+            script: './dist/apps/user/main.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
