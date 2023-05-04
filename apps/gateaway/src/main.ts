fd75bc7d88b9460697d380f2575dd24c913a1a2e
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-05-04 12:15:28 +0500

    add deploy configs

--- apps/gateaway/src/main.ts
+++ apps/gateaway/src/main.ts
@@ -7,7 +7,13 @@ import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Accept',
+        credentials: true,
+    });
     app.use(json({ limit: '100mb' }));
     app.use(compression());
 
