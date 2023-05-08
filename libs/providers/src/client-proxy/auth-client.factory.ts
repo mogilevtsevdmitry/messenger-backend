f7f89a8d735e96da3975a9ba5d5ce81fdfc8f719
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-05-08 12:49:42 +0500

    UPD: auth client

--- libs/providers/src/client-proxy/auth-client.factory.ts
+++ libs/providers/src/client-proxy/auth-client.factory.ts
@@ -1,3 +1,4 @@
+import { AuthClient } from '@contracts/services/auth';
 import { ConfigService } from '@nestjs/config';
 import { ClientProvider, ClientsModuleAsyncOptions, Transport } from '@nestjs/microservices';
 
@@ -14,7 +15,7 @@ export const authClientProvider = (config: ConfigService): ClientProvider => {
 export const authClientFactory = (): ClientsModuleAsyncOptions => {
     return [
         {
-            name: 'AUTH_SERVICE',
+            name: AuthClient.Name,
             inject: [ConfigService],
             useFactory: (config: ConfigService) => authClientProvider(config),
         },
