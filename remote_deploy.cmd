39ee5a06f40e2e3a0f0d64dc9cc0e7999b572f99
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-05-12 22:28:41 +0500

    RM: unused files

--- remote_deploy.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Замените следующие значения на свои:
-set USER=root
-set REMOTE_HOST=95.163.240.197
-set KEY_PATH="C:\Users\mogil\.ssh\messenger.ppk"
-
-REM Подключение к удаленному серверу через PuTTY и выполнение команд
-"C:/Program Files/PuTTY/putty.exe" -ssh %USER%@%REMOTE_HOST% -m remote_deploy.txt
--- remote_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Замените следующие значения на свои:
-USER="root"
-REMOTE_HOST="95.163.240.197"
-REMOTE_DIR="messenger-backend"
-
-# Подключение к удаленному серверу через SSH и выполнение команд
-ssh -t $USER@$REMOTE_HOST "cd $REMOTE_DIR && sudo ./deploy.sh"
