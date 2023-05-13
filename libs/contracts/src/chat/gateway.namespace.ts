565c6ddc6602e4ddeb63d8b5e589cc350933b92a
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-05-13 21:40:31 +0500

    ADD: private chat contract

---
+++ libs/contracts/src/chat/gateway.namespace.ts
@@ -0,0 +1,6 @@
+export namespace ChatGatewayNamespace {
+    /** Название пространства */
+    export const Namespace = 'chat';
+    /** Наименование события личного сообщения */
+    export const DirectMessage = 'chat.direct';
+}
